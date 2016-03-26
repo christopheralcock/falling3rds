298b2b158b8cd6344f5883610c2b808c34ea4e25
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-03-26 21:59:39 +0000

    colour changer extracted

--- public/falling3rdsApp.js
+++ public/falling3rdsApp.js
@@ -1,12 +1,59 @@
 var audioContext = new AudioContext();
 
 var colourCyclerApp = {
-  
-}
+  colourStage: 0,
+  addedByFirstClick: 3,
+  maxSaturation: 80,
+  maxLight: 90,
+  minSaturation: 20,
+  minLight: 20,
+  triangleSpeed: Math.random(),
+  backgroundSpeed: Math.random(),
+  colourSuperSpeed: 300,
+  colourLoops: [],
+
+  cycleColours: function(){
+    this.startColours = setInterval(this.cycleBackgroundColour,this.colourSuperSpeed);
+    this.colourLoops.push(this.startColours);
+  },
+
+  rotateColours: function(start, speed){
+    return (((this.colourStage + start - this.addedByFirstClick) * speed )+((1 - speed)*(start - this.addedByFirstClick))) % 360;
+  },
+
+  fluctuate: function(count, max){
+    if ((parseInt(count / max)) % 2 == 0){
+      return count - (parseInt(count / max) * max);
+    } else {
+      return max - (count - (parseInt(count / max) * max));
+    };
+  },
+
+  fluctuateOffset: function(count, max, min, start, speed){
+    var adjustedCount = (count * speed) + start - ((this.addedByFirstClick * speed) + min);
+    var range = max - min;
+    return this.fluctuate(adjustedCount, range) + min;
+  },
+
+  cycleBackgroundColour: function(){
+    colourCyclerApp.colourStage += 1;
+    var bgH = colourCyclerApp.rotateColours(229, colourCyclerApp.backgroundSpeed);
+    var bgS = colourCyclerApp.fluctuateOffset(colourCyclerApp.colourStage, colourCyclerApp.maxSaturation, colourCyclerApp.minSaturation, 26, colourCyclerApp.backgroundSpeed);
+    var bgL = colourCyclerApp.fluctuateOffset(colourCyclerApp.colourStage, colourCyclerApp.maxLight, colourCyclerApp.minLight, 88, colourCyclerApp.backgroundSpeed);
+    var backgroundColour = "hsl(" + bgH + ", " + bgS + "%, " + bgL + "%)";
+
+    var triH = colourCyclerApp.rotateColours(233, colourCyclerApp.triangleSpeed);
+    var triS = colourCyclerApp.fluctuateOffset(colourCyclerApp.colourStage, colourCyclerApp.maxSaturation, colourCyclerApp.minSaturation, 43, colourCyclerApp.triangleSpeed);
+    var triL = colourCyclerApp.fluctuateOffset(colourCyclerApp.colourStage, colourCyclerApp.maxLight, colourCyclerApp.minLight, 45, colourCyclerApp.triangleSpeed);
+    var triangleColour = "hsl(" + triH + ", " + triS + "%, " + triL + "%)";
+
+    document.getElementById("fullPage").style.background = backgroundColour;
+    document.getElementById("triangle-down").style.color = triangleColour;
+  }
+};
 
 var falling3rdsApp = {
   currentNumberOfParts: 0,
-  colourStage: 0,
 
   updateLoopControls: function(){
     this.updateStars();
@@ -46,56 +93,7 @@ var falling3rdsApp = {
     };
   },
 
-  addedByFirstClick: 3,
-  maxSaturation: 80,
-  maxLight: 90,
-  minSaturation: 20,
-  minLight: 20,
-  triangleSpeed: Math.random(),
-  backgroundSpeed: Math.random(),
-  colourSuperSpeed: 300,
   musicLoops: [],
-  colourLoops: [],
-
-  cycleColours: function(){
-    this.startColours = setInterval(this.cycleBackgroundColour,this.colourSuperSpeed);
-    this.colourLoops.push(this.startColours);
-  },
-
-  rotateColours: function(start, speed){
-    return (((this.colourStage + start - this.addedByFirstClick) * speed )+((1 - speed)*(start - this.addedByFirstClick))) % 360;
-  },
-
-  fluctuate: function(count, max){
-    if ((parseInt(count / max)) % 2 == 0){
-      return count - (parseInt(count / max) * max);
-    } else {
-      return max - (count - (parseInt(count / max) * max));
-    };
-  },
-
-  fluctuateOffset: function(count, max, min, start, speed){
-    var adjustedCount = (count * speed) + start - ((this.addedByFirstClick * speed) + min);
-    var range = max - min;
-    return this.fluctuate(adjustedCount, range) + min;
-  },
-
-  cycleBackgroundColour: function(){
-    falling3rdsApp.colourStage += 1;
-    var bgH = falling3rdsApp.rotateColours(229, falling3rdsApp.backgroundSpeed);
-    var bgS = falling3rdsApp.fluctuateOffset(falling3rdsApp.colourStage, falling3rdsApp.maxSaturation, falling3rdsApp.minSaturation, 26, falling3rdsApp.backgroundSpeed);
-    var bgL = falling3rdsApp.fluctuateOffset(falling3rdsApp.colourStage, falling3rdsApp.maxLight, falling3rdsApp.minLight, 88, falling3rdsApp.backgroundSpeed);
-    var backgroundColour = "hsl(" + bgH + ", " + bgS + "%, " + bgL + "%)";
-
-    var triH = falling3rdsApp.rotateColours(233, falling3rdsApp.triangleSpeed);
-    var triS = falling3rdsApp.fluctuateOffset(falling3rdsApp.colourStage, falling3rdsApp.maxSaturation, falling3rdsApp.minSaturation, 43, falling3rdsApp.triangleSpeed);
-    var triL = falling3rdsApp.fluctuateOffset(falling3rdsApp.colourStage, falling3rdsApp.maxLight, falling3rdsApp.minLight, 45, falling3rdsApp.triangleSpeed);
-    var triangleColour = "hsl(" + triH + ", " + triS + "%, " + triL + "%)";
-
-    document.getElementById("fullPage").style.background = backgroundColour;
-    document.getElementById("triangle-down").style.color = triangleColour;
-  },
-
   arpeggioNotes: [0,0,4,4,7],
   octaveDistribution: [-24,-24,-12,-12,-12,0,0,0,0,0,12,12],
   repeatPeriods: [3,4,5,5,5.5,6,7,7.5,7.5,8,9,10,11,12,13,14,15,16],
@@ -238,13 +236,13 @@ window.onload = function(){
 document.getElementById("play-button").onclick = function(){
   if (falling3rdsApp.currentNumberOfParts < 5) {
     falling3rdsApp.newMusicalPart();
-    falling3rdsApp.cycleColours();
+    colourCyclerApp.cycleColours();
   };
   falling3rdsApp.updateLoopControls();
 };
 
 document.getElementById("stop").onclick = function(){
-  clearInterval(falling3rdsApp.colourLoops.pop());
+  clearInterval(colourCyclerApp.colourLoops.pop());
   clearInterval(falling3rdsApp.musicLoops.pop());
   if (falling3rdsApp.currentNumberOfParts > 0) {
     falling3rdsApp.currentNumberOfParts -= 1;
